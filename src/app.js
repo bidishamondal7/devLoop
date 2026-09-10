99ff72e852580fb3705229e0ca99fe5b79647881
bidishamondal7 <bidisha.tuli7@gmail.com>
2026-09-11 00:25:51 +0530

    Practicing routes and route extensions

--- src/app.js
+++ src/app.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 
 //Handling server - request handler
-app.use("/test", (req, res) => {
-  res.send("Hello from the server side");
+// app.use("/", (req, res) => {
+//   res.send("Hello from root");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server side");
+// });
+
+app.get("/user", (req, res) => {
+  res.send("User data");
+});
+app.post("/user", (req, res) => {
+  res.send("User data has been created");
+});
+app.patch("/user", (req, res) => {
+  res.send("User data has been updated specific data");
+});
+
+app.put("/user", (req, res) => {
+  res.send("User data has been replaced");
+});
+app.delete("/user", (req, res) => {
+  res.send("User data has been deleted");
 });
 
 //Server is listening

2f901a0a73df63d9bbd3952b9c05e2631c636e70
bidishamondal7 <bidisha.tuli7@gmail.com>
2026-09-21 17:10:07 +0530

    Error handling practice

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+// Middlewares
 const {adminAuth, userAuth} = require("./middlewares/auth");
 app.use("/admin", adminAuth);
-app.get("/admin/getAllUser", (req, res) => {
+app.get("/admin/getAllUser", (req, res, next) => {
+  throw new Error("This is an error in getAllUser route");
   res.send("This is the getAllUser route");
 });
 app.delete("/admin/deleteUser", (req, res) => {
@@ -13,6 +15,15 @@ app.delete("/admin/deleteUser", (req, res) => {
 app.get("/user/getProfile",userAuth, (req, res) => {
   res.send("This is the getProfile route");
 });
+
+//Error handlers
+app.use("/",(err, req, res, next)=>{
+if(err){
+  res.status(500).send("Internal Server Error");
+}
+});
+
+
 //Server is listening
 app.listen(7777, () => {
   console.log("Server is running on port 7777!!!")
